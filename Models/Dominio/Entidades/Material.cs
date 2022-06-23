bb68881804b6dc2caa976b3a982eb79e3d6f17fa
yonnw <39948500+yonnw@users.noreply.github.com>
2022-06-23 17:34:14 +0100

    Create Material.cs

---
+++ Models/Dominio/Entidades/Material.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace cms_stock.Models.Dominio.Entidades
+{
+    public class Material
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("CentroCusto")]
+        public int CentroCustoId { get; set; }
+        public virtual CentroCusto CentroCusto { get; set; }
+
+        [Required]
+        [ForeignKey("Funcionario")]
+        public int FuncionarioId { get; set; }
+        public virtual Funcionario Funcionario { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DataPedido { get; set; }
+
+        public string NomeMaterial { get; set; }
+        public bool Concluido { get; set; }
+    }
+}
